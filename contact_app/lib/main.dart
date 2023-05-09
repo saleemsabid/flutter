2ce3ac4b3c0501e7f9b1a7044c45ea2974927950
saleemsabid <saleemsabid@gmail.com>
2023-05-09 15:52:05 +0500

    find a hack to display the people class data somehow

--- contact_app/lib/main.dart
+++ contact_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:contact_app/src/people.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// final Person person = people[0];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,16 +18,16 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity),
       debugShowCheckedModeBanner: false,
-      home: const Home(title: "Contact App"),
+      home: const Home(
+        title: "Contact App",
+      ),
     );
   }
 }
 
 class Home extends StatefulWidget {
   const Home({super.key, required this.title});
-
   final String title;
-
   @override
   State<Home> createState() => _HomeState();
 }
@@ -39,9 +42,64 @@ class _HomeState extends State<Home> {
       ),
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
-          return const Center(child: Text('Hello World'));
+          if (constraints.maxWidth > 800) {
+            return const WideLayout();
+          } else if (constraints.maxWidth < 500) {
+            return const NarrowLayout();
+          } else {
+            return const MidLayout();
+          }
         },
       ),
+    );
+  }
+}
+
+class WideLayout extends StatelessWidget {
+  const WideLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
+
+class MidLayout extends StatelessWidget {
+  const MidLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+    );
+  }
+}
+
+class NarrowLayout extends StatelessWidget {
+  const NarrowLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      child: const ContactTile(index: 4),
+    );
+  }
+}
+
+class ContactTile extends StatelessWidget {
+  const ContactTile({super.key, required this.index});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: const Icon(Icons.account_circle_rounded),
+      title: Text(people[index].name),
+      subtitle: Text(people[index].age),
     );
   }
 }
